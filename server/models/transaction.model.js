f20896d8d6cc54dd840ae6035358984dae023f86
Michael Gift <simiyugift@outlook.com>
2024-09-23 19:58:48 +0300

    Create transaction.model.js

---
+++ server/models/transaction.model.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const TransactionSchema = new mongoose.Schema(
+    {
+        amount: {
+            type: Number,
+            required: [true, "Please enter amount"],
+        },
+        status: {
+            type: String,
+            enum: ['Pending', 'Completed', 'Failed'],
+            required: true,
+            default: 'Pending',
+        },
+
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'User',
+            required: true,
+        },
+    }, {
+        timestamps: true,
+    }
+);
