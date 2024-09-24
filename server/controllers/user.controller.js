f179a5d8e734626d687fb612580d145ea79e8ceb
Michael Gift <simiyugift@outlook.com>
2024-09-24 20:09:46 +0300

    Add users field to Json

--- server/controllers/user.controller.js
+++ server/controllers/user.controller.js
@@ -17,7 +17,7 @@ const getAllUsers = async (req, res) => {
     try {
         const users = await User.find();
 
-        return res.status(200).json(users);
+        return res.status(200).json({users: users});
     } catch (error) {
         return res.status(500).json({message: error.message});
     }
