fb1285e3b324dc8267fdd41caa94b21d2b398f26
Michael Gift <simiyugift@outlook.com>
2024-09-24 20:09:20 +0300

    Export transaction model

--- server/models/transaction.model.js
+++ server/models/transaction.model.js
@@ -23,3 +23,6 @@ const TransactionSchema = new mongoose.Schema(
         timestamps: true,
     }
 );
+
+const Transaction = mongoose.model('Transaction', TransactionSchema);
+module.exports = Transaction;
