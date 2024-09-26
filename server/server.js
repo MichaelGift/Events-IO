89c03970c5167101780edc5c0ef52df53ac80c0e
Michael Gift <simiyugift@outlook.com>
2024-09-26 23:43:54 +0300

    Set up auth verification middleware

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const admin = require('firebase-admin');
 const cors = require('cors');
 require('dotenv').config();
 
@@ -7,11 +8,15 @@ const eventsRoute = require('./routes/event.routes');
 const transactionRoute = require('./routes/transaction.routes');
 const userRoute = require('./routes/user.routes');
 
+const authVerification = require('./middlewares/auth-verification');
+
 const app = express();
+admin.initializeApp();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
+app.use(authVerification);
 
 
 mongoose.connect(`${process.env.MONGODB_URI}`,)
